b7b4c3ce8ac48c6880f92fbbd84e17599c29e57e
Luanda Brito <49211714+luandabrito@users.noreply.github.com>
2020-05-27 15:38:12 -0300

    introduction

--- exercicio.php
+++ exercicio.php
@@ -4,23 +4,23 @@
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
     <title>Curso PHP</title>
 </head>
-<body>
+<body class="exercicio">
     <header class="cabecalho">
-        <h1>Curso PHP</h1>
-        <h2>Índice dos Exercícios</h2>
+        <h1>Exercício</h1>
+        <h2>Visualização do Exercício</h2>
     </header>
+    <nav class="navegacao">
+        <a href=<?= "teste/teste.php" ?> class="verde">Sem formatação</a>    
+        <a href="index.php" class="vermelho">Voltar</a>    
+    </nav>
     <main class="principal">
         <div class="conteudo">
-            <nav class="modulos">
-                <div class="modulo verde">
-                    <h3>Módulo 01</h3>
-                    <ul>
-                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
-                    </ul>
-                </div>
-            </nav>
+            <?php
+            include ($_GET['dir'] . "/" . $_GET['file'] . ".php");
+            ?>
         </div>
     </main>
     <footer class="rodape">
