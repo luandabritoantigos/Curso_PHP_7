eb8d50ec77fdcaf83ee58fcf7fbe892901005400
Luanda Brito <49211714+luandabrito@users.noreply.github.com>
2020-06-26 09:53:55 -0300

    sessão, cookie e alteração de css

--- sessao/gerenciando_sessao.php
+++ sessao/gerenciando_sessao.php
@@ -1,12 +1,23 @@
 <div class="titulo">Gerenciando Sessão</div>
 
 <?php
+//session_id('lq186q8ci6k0hjtui8ev2kmvf2');
 session_start();
 echo session_id();
 
 $contador = &$_SESSION['contador'];
 $contador = $contador ? $contador + 1 : 1;
 echo '<br>' . $_SESSION['contador'];
+
+if($_SESSION['contador'] % 5 ===  0) {
+    session_regenerate_id();
+}
+
+if($_SESSION['contador'] >= 15) {
+    session_destroy();
+}
+
+
 
 
 ?>
