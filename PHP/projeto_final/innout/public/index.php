aa2d1219995e7154a71445fb1b800197837cf6ec
Luanda Brito <49211714+luandabrito@users.noreply.github.com>
2020-07-15 09:46:22 -0300

    update projeto final

--- PHP/projeto_final/innout/public/index.php
+++ PHP/projeto_final/innout/public/index.php
@@ -3,8 +3,8 @@
 require_once (dirname(__FILE__, 2) . '/src/config/config.php');
 require_once (dirname(__FILE__, 2) . '/src/models/User.php');
 
-$user = new User(['']);
-print_r($user);
+$user = new User(['name' => 'Maria', 'email' => 'maria@maria.com']);
 
+echo $user->getSelect(['name' => 'Chaves'], 'name, email');
 
 ?>
--- PHP/projeto_final/innout/src/models/Model.php
+++ PHP/projeto_final/innout/src/models/Model.php
@@ -12,16 +12,42 @@ class Model {
     public function loadFromArray($arr) {
         if($arr) {
             foreach($arr as $key => $value) {
-                $this->set($key, $value);
+                $this->$key = $value;
             }
         }
     }
 
-    public function get($key) {
+    public function __get($key) {
         return $this->values[$key];
     }
 
-    public function set($key, $value) {
+    public function __set($key, $value) {
         $this->values[$key] = $value;
+    }
+
+    public static function getSelect($filters = [], $columns = '*') {
+        $sql = "SELECT ${columns} FROM " . static::$tableName . static::getFilters($filters);
+        return $sql;
+    }
+
+    private static function getFilters($filters) {
+        $sql = '';
+        if(count($filters) > 0) {
+            $sql .= " WHERE 1 = 1";
+            foreach ($filters as $columns => $value) {
+            $sql .= " AND ${columns} = " . static:: getFormatedValue($value);
+            }
+        }
+        return $sql;
+    }
+
+    private static function getFormatedValue($value) {
+        if(is_null($value)) {
+            return "null";
+        } elseif(gettype($value) === 'string') {
+            return "'${value}'";
+        } else {
+            return $value;
+        }
     }
 }
