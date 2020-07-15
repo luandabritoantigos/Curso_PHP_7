a3e1e939b2923663f26165eb4041b23fa700ef99
Luanda Brito <49211714+luandabrito@users.noreply.github.com>
2020-07-15 14:41:20 -0300

    update login - projeto final

--- PHP/projeto_final/innout/public/index.php
+++ PHP/projeto_final/innout/public/index.php
@@ -1,8 +1,17 @@
 <?php
 
 require_once (dirname(__FILE__, 2) . '/src/config/config.php');
-require_once (dirname(__FILE__, 2) . '/src/views/login.php');
+//require_once (VIEW_PATH . '/login.php');
 
+require_once(MODEL_PATH . '/Login.php');
 
+$login = new Login(['email' => 'admin@cod3r.com.br', 'password' => 'b']);
+
+try {
+    $login->checkLogin();
+    echo 'Deu certo';
+} catch(Exception $e) {
+    echo "Problema no login";
+}
 
 ?>
--- PHP/projeto_final/innout/src/config/config.php
+++ PHP/projeto_final/innout/src/config/config.php
@@ -6,9 +6,12 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 // Pastas
 
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
 
-require_once (realpath(dirname(__FILE__) . '/database.php'))
 
+// Arquivos
+require_once (realpath(dirname(__FILE__) . '/database.php'));
+require_once (realpath(MODEL_PATH . '/Model.php'));
 
 
 
